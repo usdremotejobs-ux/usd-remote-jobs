810cfddd66a98ccd0175025623a17ca9830ce556
usdremotejobs-ux <usdremotejobs@gmail.com>
2026-01-05 16:48:39 +0530

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate, useLocation } from "react-router-dom"
 import { useAuth } from "../context/AuthContext"
 
 export default function ProtectedRoute({ children, requireActiveSubscription }) {
-  const { user, authLoading, subscription } = useAuth()
+  const { user,subscription,authLoading} = useAuth()
   const location = useLocation()
 
   if (authLoading) {
